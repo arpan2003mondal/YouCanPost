8d961de12f7480b3da268a3c5b277dcb8e5c065e
Arpan Mondal <arpan2003mondal@gmail.com>
2024-11-23 01:14:39 +0530

    final changes completed

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
-
 const dbgr = require("debug")("development:mongoose"); 
-
 const config = require("config");
 
 mongoose
-.connect(`${config.get("MONGODB_URI")}/postCreation`)
-.then(function(){
-    dbgr("connected");
-})
-.catch(function(err){
-    dbgr(err);
-});
-
-
+    .connect(config.get("MONGODB_URI"), { dbName: "PostCreation" }) // Specify the database name
+    .then(() => dbgr("MongoDB connected"))
+    .catch((err) => dbgr("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
+
+
+
