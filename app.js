cc991839a7899f0523ec8b6388ec650d81b3ed95
Arpan Mondal <arpan2003mondal@gmail.com>
2024-11-23 12:11:01 +0530

    views path added

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const indexRouter = require("./routes/index");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
+app.set("views", path.join(__dirname, "views"));
+
 app.use(cookieParser());
 app.use(
     expressSession({
